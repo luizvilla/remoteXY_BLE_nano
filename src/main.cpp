dbacf0e6ec3c65e68648c8317daf17b6452a51b0
Luiz-Fernando Lavado-Villa <lflavado@tu-te-calmes>
2021-05-30 13:16:05 +0200

    Made some changes to the serial and now it rights correctly on the hardware serial

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,19 @@
 //////////////////////////////////////////////
 
 // RemoteXY select connection mode and include library 
+//#define REMOTEXY__DEBUGLOGS Serial
+
+// -----------------------SUGGESTED CODE FROM REMOTEXY FORUM -------------------------
+// #define REMOTEXY_MODE__SOFTSERIAL
+// #include <SoftwareSerial.h>
+// #include <RemoteXY.h>
+
+// // RemoteXY connection settings 
+// #define REMOTEXY_SERIAL_RX 0 // or try 1
+// #define REMOTEXY_SERIAL_TX 1  // or try 0
+// #define REMOTEXY_SERIAL_SPEED 9600
+
+// // RemoteXY select connection mode and include library 
 #define REMOTEXY_MODE__HARDSERIAL
 
 #include <RemoteXY.h>
@@ -14,20 +27,19 @@
 #define REMOTEXY_SERIAL_SPEED 9600
 
 
+// ----------------------------------------------------------------------------
+
 // RemoteXY configurate  
 #pragma pack(push, 1)
 uint8_t RemoteXY_CONF[] =
-  { 255,0,0,1,0,30,0,11,13,0,
-  66,130,41,26,20,16,2,26,129,0,
-  42,21,18,3,17,77,111,116,111,114,
-  32,83,112,101,101,100,0 };
+  { 255,1,0,0,0,13,0,11,13,0,
+  1,0,42,24,12,12,2,31,88,0 }; 
   
 // this structure defines all the variables and events of your control interface 
 struct {
 
     // output variables
-  int8_t pwm_out; // =0..100 level position 
-
+  uint8_t button_1; // =1 if button pressed, else =0 
     // other variable
   uint8_t connect_flag;  // =1 if wire connected, else =0 
 
@@ -41,16 +53,20 @@ struct {
 
 void setup() {
   // put your setup code here, to run once:
-   RemoteXY_Init ();  
 
   Serial.begin(9600);
   Serial.println("INITIALIZING");
+
+  RemoteXY_Init ();  
+
 }
 
 void loop() {
 
-  RemoteXY_Handler (); 
-  RemoteXY.pwm_out = 10;
+  RemoteXY_Handler ();
+
+  // uint8_t bte = 0xA;
+  // Serial.print(bte);
 
 
   // put your main code here, to run repeatedly:
